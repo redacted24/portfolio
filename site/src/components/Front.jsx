2b11a3a40bd7b787746e149ec4885ed8e68e574a
redacted24 <huangq4@outlook.com>
2024-03-04 21:24:15 -0500

    set radials

--- site/src/components/Front.jsx
+++ site/src/components/Front.jsx
@@ -1,7 +1,17 @@
 const Front = () => {
     return (
-        <div>
-            Front Page
+        <div id='frontContainer'>
+            <img src='../../assets/bigRadial.png' alt='bigRadial.png' id='bigRadial'/>
+            <img src='../../assets/smallRadial.png' alt='smallRadial.png' id='smallRadial'/>
+            <div className="spaceEvenly">
+                <div id='mainTitle'>
+                    <span>hey, <br/>I'm <span className="redGlow">Qiuyu</span>.<br/></span>
+                    <span>a programmer</span>
+                </div>
+                <div id='codeBox'>
+
+                </div>
+            </div>
         </div>
     )
 }
