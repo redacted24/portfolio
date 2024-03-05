08df6015ce04b1448ae0050e399bea49bcbc47d6
redacted24 <huangq4@outlook.com>
2024-03-04 21:54:43 -0500

    finished about me section

--- site/src/components/Front.jsx
+++ site/src/components/Front.jsx
@@ -23,6 +23,25 @@ const Front = () => {
 
             {/* About Me */}
             <div>
+                <h2 className="headings" >About me</h2>
+                <div className="gotham font24px spaceEvenly">
+                    <div>
+                        <p id='aboutmep1'>I am currently a student at <span className="redGlow">Marianopolis College</span> looking to study SE at UWaterloo.</p>
+                        <p id='aboutmep2'>Here are some of the things I've worked with:</p>
+                        <div className="flex">
+                            <ul>
+                                <li>React.js</li>
+                                <li>Javascript ES6+</li>
+                            </ul>
+                            <ul>
+                                <li>Python</li>
+                                <li>Express</li>
+                            </ul>
+                        </div>
+                        <p id='aboutmep3'>Outside of school, I do snowboarding, photography and I enjoy playing/developing <span className="redGlow">video games.</span></p>
+                    </div>
+                    <img src='../../assets/photome.jpg' alt='photo of me' id='myPhoto'/>
+                </div>
             </div>
         </div>
     )
