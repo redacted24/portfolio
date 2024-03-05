bce863636b1e6d4f7a90d5d36370531cec57853e
redacted24 <huangq4@outlook.com>
2024-03-04 21:38:00 -0500

    finished codebox layout

--- site/src/components/Front.jsx
+++ site/src/components/Front.jsx
@@ -3,14 +3,26 @@ const Front = () => {
         <div id='frontContainer'>
             <img src='../../assets/bigRadial.png' alt='bigRadial.png' id='bigRadial'/>
             <img src='../../assets/smallRadial.png' alt='smallRadial.png' id='smallRadial'/>
-            <div className="spaceEvenly">
+
+            {/* Title Page and CodeBox */}
+            <div className="flex">
                 <div id='mainTitle'>
                     <span>hey, <br/>I'm <span className="redGlow">Qiuyu</span>.<br/></span>
                     <span>a programmer</span>
                 </div>
                 <div id='codeBox'>
-
+                    <div>
+                        <p>
+                            // Loading...<br/>
+                            Enumerating objects... (30432/31923)<br/>
+                            Hi! I'm Qiuyu <br/>
+                        </p>
+                    </div>
                 </div>
+            </div>
+
+            {/* About Me */}
+            <div>
             </div>
         </div>
     )
